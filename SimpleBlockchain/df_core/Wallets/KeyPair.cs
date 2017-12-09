8379c2f0f3959c39a34de1cf4dc4063b7a74dae3
dotFund <dotFund@outlook.com>
2017-12-09 09:00:24 +0800

    updating wallet

---
+++ SimpleBlockchain/df_core/Wallets/KeyPair.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SimpleBlockchain.Core;
+using SimpleBlockchain.Cryptography;
+
+namespace SimpleBlockchain.Wallets
+{
+    public class KeyPair : IEquatable<KeyPair>
+    {
+        public readonly byte[] PrivateKey;
+        public readonly Cryptography.ECC.ECPoint PublicKey;
+        public readonly UInt160 PublicKeyHash;
+
+        public bool Equals(KeyPair other)
+        {
+            if (ReferenceEquals(this, other)) return true;
+            if (ReferenceEquals(null, other)) return false;
+            return PublicKeyHash.Equals(other.PublicKeyHash);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as KeyPair);
+        }
+
+        public override int GetHashCode()
+        {
+            return PublicKeyHash.GetHashCode();
+        }
+
+
+        public KeyPair(byte[] privateKey)
+        {
+            if (privateKey.Length != 32 && privateKey.Length != 96 && privateKey.Length != 104)
+                throw new ArgumentException();
+            this.PrivateKey = new byte[32];
+            Buffer.BlockCopy(privateKey, privateKey.Length - 32, PrivateKey, 0, 32);
+            if (privateKey.Length == 32)
+            {
+                this.PublicKey = Cryptography.ECC.ECCurve.Secp256r1.G * privateKey;
+            }
+            else
+            {
+                this.PublicKey = Cryptography.ECC.ECPoint.FromBytes(privateKey, Cryptography.ECC.ECCurve.Secp256r1);
+            }
+            this.PublicKeyHash = PublicKey.EncodePoint(true).ToScriptHash();
+#if NET461
+            ProtectedMemory.Protect(PrivateKey, MemoryProtectionScope.SameProcess);
+#endif
+        }
+
+        public IDisposable Decrypt()
+        {
+#if NET461
+            return new ProtectedMemoryContext(PrivateKey, MemoryProtectionScope.SameProcess);
+#else
+            return new System.IO.MemoryStream(0);
+#endif
+        }
+
+        public string Export()
+        {
+            using (Decrypt())
+            {
+                byte[] data = new byte[34];
+                data[0] = 0x80;
+                Buffer.BlockCopy(PrivateKey, 0, data, 1, 32);
+                data[33] = 0x01;
+                string wif = data.Base58CheckEncode();
+                Array.Clear(data, 0, data.Length);
+                return wif;
+            }
+        }
+
+        public string Export(string passphrase)
+        {
+            using (Decrypt())
+            {
+                UInt160 script_hash = Contract.CreateSignatureRedeemScript(PublicKey).ToScriptHash();
+                string address = Wallet.ToAddress(script_hash);
+                byte[] addresshash = Encoding.ASCII.GetBytes(address).Sha256().Sha256().Take(4).ToArray();
+                byte[] derivedkey = SCrypt.DeriveKey(Encoding.UTF8.GetBytes(passphrase), addresshash, 16384, 8, 8, 64);
+                byte[] derivedhalf1 = derivedkey.Take(32).ToArray();
+                byte[] derivedhalf2 = derivedkey.Skip(32).ToArray();
+                byte[] encryptedkey = XOR(PrivateKey, derivedhalf1).AES256Encrypt(derivedhalf2);
+                byte[] buffer = new byte[39];
+                buffer[0] = 0x01;
+                buffer[1] = 0x42;
+                buffer[2] = 0xe0;
+                Buffer.BlockCopy(addresshash, 0, buffer, 3, addresshash.Length);
+                Buffer.BlockCopy(encryptedkey, 0, buffer, 7, encryptedkey.Length);
+                return buffer.Base58CheckEncode();
+            }
+        }
+
+        private static byte[] XOR(byte[] x, byte[] y)
+        {
+            if (x.Length != y.Length) throw new ArgumentException();
+            return x.Zip(y, (a, b) => (byte)(a ^ b)).ToArray();
+        }
+    }
+}
---
+++ SimpleBlockchain/df_core/Wallets/Wallet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using SimpleBlockchain.Cryptography;
+
+namespace SimpleBlockchain.Wallets
+{
+    public abstract class Wallet : IDisposable
+    {
+        public event EventHandler BalanceChanged;
+
+        public static readonly byte AddressVersion = Settings.Default.AddressVersion;
+
+        private readonly string path;
+        private readonly byte[] iv;         //what is this?
+        private readonly byte[] masterKey;  //what is this?
+        private readonly Dictionary<UInt160, KeyPair> keys;
+        public void Dispose()
+        {
+
+        }
+
+        public static string ToAddress(UInt160 scriptHash)
+        {
+            byte[] data = new byte[21];
+            data[0] = AddressVersion;
+            Buffer.BlockCopy(scriptHash.ToArray(), 0, data, 1, 20);
+            return data.Base58CheckEncode();
+        }
+    }
+}
